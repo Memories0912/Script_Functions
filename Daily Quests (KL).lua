6a940c43713f7ae31885e0e97b569069fe73143a
Memories0912 <85147023+Memories0912@users.noreply.github.com>
2025-01-29 14:37:54 +0700

    Create Daily Quests (KL).lua

---
+++ Daily Quests (KL).lua
@@ -0,0 +1,38 @@
+DailyQuests = {}
+if game.PlaceId == 4520749081 then
+    DailyQuests = {
+        {"Kill 20 Soldiers", 1, "Soldier [Lv. 1]"},
+        {"Forget and Forgot" , 5},
+        {"Venture Lagoons!", 10},
+        {"Tomb Raiding!", 50, "Shadow Master [Lv. 1650]"},
+        {"Dead Walking", 100, "Zombie [Lv. 1500]"},
+        {"Kill 4 King Snow", 500, "King Snow [Lv. 450]"},
+        {"Kill 10 Soldier Fishman", 2000, "Soldier Fishman [Lv. 2150]"}
+    }
+elseif game.PlaceId == 6381829480 then
+    DailyQuests = {
+        {"Under The Sea~", 3000},
+        {"I'm not,YOU ARE!", 3000},
+        {"Box Box!", 3000},
+        {"Bone Hunter", 3000},
+        {"Order Disobey", 3000},
+        {"Dead Above", 3000},
+        {"Catch me,If you can", 3000},
+        {"The Lost Book", 3800},
+        {"Is it Thriller?", 3050, "Skull Pirate [Lv. 3050]"},
+        {"Left to Dead", 3000, "Dead Troupe Captain [Lv. 3850]"},
+        {"One More Time", 3000, "Joey [Lv. 3000]"},
+        {"Believer", 3500, "Hefty [Lv. 3550]"}
+    }
+elseif game.PlaceId == 15759515082 then
+    DailyQuests = {
+        {"The Pillar", 4000},
+        {"Hide'n Seek!", 4000},
+        {"Mossy Must Gone", 4000},
+        {"Rolling in the Depth", 4000, "Deep Diver [Lv. 4000]"},
+        {"Redemption", 4000, "Fugitive [Lv. 4050]"},
+        {"Into the Bubble-Verse", 4000, "Cyborg Gorilla [Lv. 4375]"},
+        {"Until Pond", 4000, "Ocean Gladiator [Lv. 4500]"},
+    }
+end
+return DailyQuests
